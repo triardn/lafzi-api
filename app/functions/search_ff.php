4e0e7d16f98296b0b0a56e3522a52c6a1ec77929
Tri Ardini <tr.ardn@gmail.com>
2018-09-04 17:45:46 +0700

    modify include

--- app/functions/search_ff.php
+++ app/functions/search_ff.php
@@ -2,9 +2,9 @@
 
 // pencari, flat file version
 
-include_once '../lib/trigram.php';
-include_once '../lib/array_utility.php';
-include_once '../lib/doc_class.php';
+include_once 'app/function/lib/trigram.php';
+include_once 'app/function/lib/array_utility.php';
+include_once 'app/function/lib/doc_class.php';
 
 // fungsi pencari
 // param  : $query_final yang siap cari (sudah melalui pengodean fonetik)
@@ -12,10 +12,10 @@ include_once '../lib/doc_class.php';
 //          $post_list_filename nama file posting list
 //          $score_order true jika ingin menghitung keterurutan kemunculan term
 // return : array of found_doc object
-function search($query_final, $term_list_filename, $post_list_filename, $score_order = true, $filtered = true, $filter_threshold = 0.8) {
-
+function search($query_final, $term_list_filename, $post_list_filename, $score_order = true, $filtered = true, $filter_threshold = 0.8)
+{
     // baca seluruh term list simpan dalam hashmap
-    $term_hashmap = array();
+    $term_hashmap = [];
     $term_list = fopen($term_list_filename, 'r');
 
     while (($line = fgets($term_list, 32)) !== false) {
@@ -23,8 +23,8 @@ function search($query_final, $term_list_filename, $post_list_filename, $score_o
         $term_hashmap[$term] = intval($offset);
     }
 
-    fclose($term_list);    
-    
+    fclose($term_list);
+
     // akses posting list
     $post_list_file = new SplFileObject($post_list_filename);
 
@@ -32,26 +32,26 @@ function search($query_final, $term_list_filename, $post_list_filename, $score_o
     $query_trigrams = trigram_frekuensi_posisi($query_final);
     $query_trigrams_count = count($query_trigrams);
     $query_trigrams_count_all = strlen($query_final) - 2;
-    
+
     if ($query_trigrams_count <= 0 || $query_trigrams_count_all <= 0) {
-        return array();
+        return [];
     };
-    
-    $matched_posting_lists = array();
-    $matched_docs = array();
+
+    $matched_posting_lists = [];
+    $matched_docs = [];
 
     // untuk setiap trigram dari query
     foreach ($query_trigrams as $query_trigram => $qtfp) {
         list($qt_freq, $qt_pos) = $qtfp;
-        
-        if (isset($term_hashmap[$query_trigram])) {    
+
+        if (isset($term_hashmap[$query_trigram])) {
             // ambil posting list yang sesuai untuk trigram ini
             $post_list_file->fseek($term_hashmap[$query_trigram]);
             $matched_posting_lists = explode(';', trim($post_list_file->current()));
 
             // untuk setiap posting list untuk trigram ini
             foreach ($matched_posting_lists as $data) {
-                list ($doc_id, $term_freq, $term_pos) = explode(':', $data);
+                list($doc_id, $term_freq, $term_pos) = explode(':', $data);
                 $term_pos = explode(',', $term_pos);
                 //$term_pos = reset(explode(',', $term_pos));
 
@@ -66,42 +66,48 @@ function search($query_final, $term_list_filename, $post_list_filename, $score_o
 
                 $matched_docs[$doc_id]->matched_terms[$query_trigram] = $term_pos; // $term_pos is an array
             }
-
         }
     }
-    
+
     // bila difilter, diambil cuma yang sekian % trigramnya cocok
-    $filtered_docs = array();
+    $filtered_docs = [];
     $min_score = $filter_threshold * (strlen($query_final) - 2);
-    
+
     // pemberian skor berdasarkan jumlah trigram yang sama dan keterurutan
     if ($score_order) {
         foreach ($matched_docs as $doc_found) {
             $doc_found->matched_terms_count_score = $doc_found->matched_trigrams_count;
-            
+
             $LIS = longest_contiguous_subsequence(array_values_flatten($doc_found->matched_terms)); // LIS_sequence(array_values_flatten($doc_found->matched_terms));
-            
+
             $doc_found->matched_terms_order_score = count($LIS);
             $doc_found->LIS = $LIS;
             $doc_found->matched_terms_contiguity_score = reciprocal_diff_average($LIS);
-            
-            $doc_found->score = $doc_found->matched_terms_order_score * $doc_found->matched_terms_contiguity_score;            
 
-            if ($filtered) if ($doc_found->matched_trigrams_count >= $min_score) $filtered_docs[] = $doc_found;
+            $doc_found->score = $doc_found->matched_terms_order_score * $doc_found->matched_terms_contiguity_score;
+
+            if ($filtered) {
+                if ($doc_found->matched_trigrams_count >= $min_score) {
+                    $filtered_docs[] = $doc_found;
+                }
+            }
         }
     } else {
         foreach ($matched_docs as $doc_found) {
             $doc_found->matched_terms_count_score = $doc_found->matched_trigrams_count;
             $doc_found->score = $doc_found->matched_terms_count_score;
-            
-            if ($filtered) if ($doc_found->matched_trigrams_count >= $min_score) $filtered_docs[] = $doc_found;
+
+            if ($filtered) {
+                if ($doc_found->matched_trigrams_count >= $min_score) {
+                    $filtered_docs[] = $doc_found;
+                }
+            }
         }
     }
 
     if ($filtered) {
-        return $filtered_docs;        
+        return $filtered_docs;
     } else {
         return $matched_docs;
     }
-    
 }
